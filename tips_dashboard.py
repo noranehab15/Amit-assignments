2d67a67f38acbcb561165f60c56b2bf6ba8d4fe8
noranehab15 <noranehab723@gmail.com>
2025-01-18 03:25:33 +0200

    Update tips_dashboard.py

--- tips_dashboard.py
+++ tips_dashboard.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 
-uploaded_file= r"C:\Users\HP\Desktop\project tips\data\tips (1).csv"
+uploaded_file= r"tips (1).csv"
 data=pd.read_csv(uploaded_file)
 
 st.title("Tips Data Dashboard")
